0a9d339f7b0bf0e7066cb4c92836c29676458b26
LUTFI IMAM TFA <lutfiimam81@gmail.com>
2026-04-02 03:46:07 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'etgcvmbpzkvqbnjohuvn.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
